e4c144e9ce554d166fde838915ee361068522437
marchel <marioniko001@gmail.com>
2022-01-05 13:55:26 +0200

    implemented memoization

--- random-solutions/recursiveFibonacci.js
+++ random-solutions/recursiveFibonacci.js
@@ -1,7 +1,18 @@
- function fibonacci(n){
-    if(n < 2){
-        return n;
+ function fibonacci(){
+    let cache = {};
+    return function fib(n){
+        if(n in cache){
+            return cache[n];
+        } else {
+            if(n < 2){
+                return n;
+            } else {
+               cache[n] = fib(n-1) + fib(n-2); //storing the result in the cache
+               return cache[n];
+            }
+        }
     }
-    return fibonacci(n-1) + fibonacci(n-2);
  }
- fibonacci(8);
+ 
+const fasterFibonacci = fibonacci();
+console.log(fasterFibonacci(10));
